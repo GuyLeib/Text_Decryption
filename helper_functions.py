00b0b01b8bcd5a1fecfb817d61dc523b60897591
ShirAmit96 <Shiramit96@gmail.com>
2023-05-15 20:03:21 +0300

    edited create permutation function

--- helper_functions.py
+++ helper_functions.py
@@ -1,15 +1,20 @@
 import itertools
-
-
+import  random
+population_size = 1000
 # This function creates all the possible permutations of the cipher:
 def create_permutations():
     # create a list of dictionaries:
     permutations = []
+    global population_size
+    counter=0
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    for permutation in itertools.permutations(letters):
+    letters_for_perm=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in range(population_size):
+        random.shuffle(letters_for_perm)
         dictionary = {}
-        for i, letter in enumerate(letters):
-            dictionary[letter] = permutation[i]
+
+        for j, letter in enumerate(letters):
+            dictionary[letter] = letters_for_perm[j]
         permutations.append(dictionary)
     return permutations
 
